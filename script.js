709037222adb567c04f6c769c624b85ff39bfd34
abdulmoizazher <abdulmoizahzer@gmail.com>
2024-06-14 20:34:03 +0500

    almost done

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
 
 
-
+function formation (n)
+{
 
 let container = document.createElement("div")
 container.setAttribute("class", "container");
 
 document.body.appendChild(container)
- 
+ console.log("hi")
 
-for(let  i = 1; i <= 225 ; i++){
-    var element = document.createElement("div")
+for(let  i = 1; i <= n**2 ; i++){
+    let element = document.createElement("div")
     element.setAttribute ("class" ,"screen")
     container.appendChild(element);
+    console.log("jo")
     
 }
+}
 
+formation(15)
+
+function paint (){
+    let screen = document.getElementsByClassName("screen");
+
+    for( let i = 0; i < screen.length;i++)
+    {
+    screen[i].addEventListener('mouseover', color )
+    }
+
+    function color () 
+    {
+        this.style.backgroundColor = "green";
+        console.log("working")
+
+    }
+}
+function reset () {
+    let number = prompt("enter the number of squares ")
+    if(number > 100 )
+    {
+        alert("enter a value less than 100")
+    }
+    else if(number < 1){
+        //pass;
+    }
+    else
+    {
+        let container = document.querySelector(".container");
+        container.remove();
+        console.log(number)
+        formation(number);
+        let divide = 600/number;
+
+        let screen = document.querySelectorAll(".screen");
+
+    for (let i = 0; i < screen.length; i++) {
+    screen[i].style.width = divide + 'px';
+    screen[i].style.height = divide + 'px';
+    }
+    paint()
+    }
+}
